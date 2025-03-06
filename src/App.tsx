8468a70ff9734b50a1589261f77c659f79e7e336
Manuel <manuel.ma@posteo.net>
2025-03-06 17:44:57 +0100

    Refactor film components: replace SearchBar with Search component and update FilmList to accept films as props. Avoiding "Latest Films" option.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from "react";
 import Header from "./component/Header";
-import SearchBar from "./component/SearchBar";
-import FilmList from "./component/FilmList";
+import Search from "./component/Search";
 
 function App() {
   return (
     <div className="container mx-auto">
       <Header />
-      <SearchBar />
-      <FilmList
-        name="Popular"
-        sortBy="popularity.desc"
-        minVotes={0}
-        limit={8}
-      />
-      <FilmList
-        name="Latest releases"
-        sortBy="primary_release_date.desc"
-        minVotes={10}
-        limit={8}
-      />
+      <Search />
     </div>
   );
 }
---
+++ src/component/Search.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import FilmList from "./FilmList";
+
+function Search(props: any) {
+  //TODO: move this
+  const api_key: string | undefined = "8f781d70654b5a6f2fa69770d1d115a3";
+
+  const [films, setFilms] = React.useState([]);
+  const [input, setInput] = React.useState("");
+  const [title, setTitle] = React.useState("");
+
+  async function searchFilms() {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?include_adult=false&include_video=false&sort_by=${props.sortBy}&api_key=${api_key}&page=1&query=${input}`,
+      );
+      const data = await response.json();
+      setFilms(data.results.slice(0, props.limit));
+      console.log(data.results.slice(0, props.limit));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function fetchFilms(sort: string, limit: number, minVotes: number) {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&sort_by=${sort}&vote_count.gte=${minVotes}&api_key=${api_key}&page=1`,
+      );
+      const data = await response.json();
+      setFilms(data.results.slice(0, limit));
+      console.log(data.results.slice(0, limit));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    if (input == "") {
+      setTitle("Popular");
+      fetchFilms("popularity.desc", 12, 100);
+    } else {
+      setTitle("Search Results");
+      searchFilms();
+    }
+  }, [input]);
+
+  return (
+    <>
+      <form className="y-2 py-1 px-3 w-[50%] justify-self-center flex items-center space-x-0 m rounded-2xl bg-[var(--color-3)]">
+        <MagnifyingGlassIcon className="size-5 text-[var(--color-1)] shadow-white" />
+        <input
+          type="text"
+          className="px-3 py-2 rounded w-full outline-none"
+          placeholder="Search films..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </form>
+      <h1 className="my-10 ml-1 font-bold text-lg text-[var(--color-1)] border-b-2 border-[var(--color-2)]">
+        {title}
+      </h1>
+      <FilmList films={films} />
+    </>
+  );
+}
+
+export default Search;
